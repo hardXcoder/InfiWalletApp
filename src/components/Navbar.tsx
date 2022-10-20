78d5f09edd85cf4c88d032e4bb568d3b7d8fceae
hardXcoder <ujjwal451@gmail.com>
2022-10-20 19:32:01 +0530

    Initialised multisigWallet Solana

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import styled from "styled-components";
+
+import logo from "../img/logo.svg";
+
+import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
+
+const ConnectButton = styled(WalletDialogButton)``;
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        {/* <img src={logo} /> */}
+        <h3 className="logo">InfinityWallet</h3>
+
+        <div className="flex items-center gap-3">
+          <a
+            href="https://magiceden.io/marketplace/"
+            className="min-w-[150px] min-h-[46px] rounded-full flex gap-1 items-center justify-center degraded"
+          >
+            <p className="font-mono font-[700]">Connect Wallet</p>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+export default Navbar;
